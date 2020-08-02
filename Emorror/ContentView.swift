65952b088b1c6b2dbc301039489e684863bf4c02
Mostfa <almme111@gmail.com>
2020-08-02 02:19:14 +0200

    Initial Commit

---
+++ Emorror/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emorror
+//
+//  Created by mostfa on 8/2/20.
+//  Copyright © 2020 mostfaE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
