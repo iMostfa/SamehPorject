5f413c6ef553d1db2a3203135c72bcbd2eea0217
Mostfa <almme111@gmail.com>
2020-08-02 05:36:47 +0200

    init

--- Emorror/Views/DetailsView.swift
+++ Emorror/Views/DetailsView.swift
@@ -8,14 +8,15 @@
 
 import SwiftUI
 
-struct DetailsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct DetailsView_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailsView()
-    }
-}
+//struct DetailsView: View {
+//  var user: FirebaseUser
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
+//
+//struct DetailsView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DetailsView()
+//    }
+//}
